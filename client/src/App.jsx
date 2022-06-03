7cb369eb653b695154605f0a4c2123934ebe93a7
slauko <slauko@users.noreply.github.com>
2022-06-03 20:16:59 +0200

    desktop layout, unstyled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,79 @@
 import React from 'react';
+import './App.scss';
 
 const Mobile = () => {
 	const [material, setMaterial] = React.useState('');
 	const [chargenr, setChargenr] = React.useState('');
 	const [amount, setAmount] = React.useState('');
 
+	const handleButtonClick = () => {
+		//TODO: send data to backend
+		if (material && chargenr && amount) {
+			console.log(material, chargenr, amount);
+		}
+	};
+
 	return (
 		<div className='Mobile'>
 			<p>Material Erfassung:</p>
 			<div className='Inputs'>
 				<div className='Dropdown'>
-					<label>Material:</label>
-					<select>
-						<option>Material1</option>
-						<option>Material2</option>
-						<option>Material3</option>
+					<select
+						onChange={(e) => {
+							setMaterial(e.target.value);
+						}}
+						defaultValue=''
+					>
+						<option value='' disabled>
+							Material auswählen
+						</option>
+						<option value='1'>Material1</option>
+						<option value='2'>Material2</option>
+						<option value='3'>Material3</option>
 					</select>
 				</div>
 				<div className='Fields'>
-					<input type='text' placeholder='Chargen-Nr.' />
-					<input type='number' placeholder='Menge' />
+					<input
+						type='text'
+						placeholder='Chargen-Nr.'
+						onChange={(e) => {
+							setChargenr(e.target.value);
+						}}
+					/>
+					<input
+						type='number'
+						placeholder='Menge'
+						onChange={(e) => {
+							setAmount(e.target.value);
+						}}
+					/>
 				</div>
 				<div className='Buttons'>
-					<input type='button' value='Speichern' />
+					<input type='button' value='Speichern' onClick={handleButtonClick} />
 				</div>
 			</div>
 		</div>
 	);
 };
 
-const Desktop = () => {
+const Desktop = ({data, setData}) => {
 	return (
 		<div className='Desktop'>
-			<p>Desktop</p>
+			<p>Material Erfassung:</p>
+			<div className='Grid'>
+				<div className='Header'>
+					<input type='text' disabled placeholder='Material' />
+					<input type='text' disabled placeholder='Changen-Nr.' />
+					<input type='text' disabled placeholder='Menge' />
+				</div>
+
+				{/*TODO: Map over DB data below*/}
+				<div className='Row'>
+					<input type='text' />
+					<input type='text' />
+					<input type='text' />
+				</div>
+			</div>
 		</div>
 	);
 };
