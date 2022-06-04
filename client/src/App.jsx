08e596379e4ac1d2971ff4121a6f8f5b063b4f98
slauko <slauko@users.noreply.github.com>
2022-06-04 11:42:55 +0200

    connected DB

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import './App.scss';
 
-const Mobile = () => {
+const API_URL = process.env.REACT_APP_API_URL + '/api/data/';
+const getDataFromDB = async () => {
+	const res = await fetch(API_URL);
+	const data = await res.json();
+	return data;
+};
+
+const Mobile = ({data, setData}) => {
 	const [material, setMaterial] = React.useState('');
 	const [chargenr, setChargenr] = React.useState('');
 	const [amount, setAmount] = React.useState('');
 
 	const handleButtonClick = () => {
-		//TODO: send data to backend
 		if (material && chargenr && amount) {
-			console.log(material, chargenr, amount);
+			//add new data to backend
+			fetch(API_URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					material: material,
+					chargenr: chargenr,
+					amount: amount,
+				}),
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					//add new data to state after added successfully
+					getDataFromDB().then((data) => {
+						setData(data);
+					});
+				})
+				.catch((err) => {
+					console.log(err);
+				});
 		}
 	};
 
@@ -27,9 +54,9 @@ const Mobile = () => {
 						<option value='' disabled>
 							Material auswählen
 						</option>
-						<option value='1'>Material1</option>
-						<option value='2'>Material2</option>
-						<option value='3'>Material3</option>
+						<option value='Material1'>Material1</option>
+						<option value='Material2'>Material2</option>
+						<option value='Material3'>Material3</option>
 					</select>
 				</div>
 				<div className='Fields'>
@@ -56,30 +83,106 @@ const Mobile = () => {
 	);
 };
 
+const DataRow = ({data, setData}) => {
+	const [material, setMaterial] = React.useState(data.material);
+	const [chargenr, setChargenr] = React.useState(data.chargenr);
+	const [amount, setAmount] = React.useState(data.amount);
+
+	const updateData = (newData, id) => {
+		//send update data to backend
+		fetch(API_URL + id, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newData),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				//update state after update successfully
+				getDataFromDB().then((data) => {
+					setData(data);
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	const handleMaterialChange = (e) => {
+		setMaterial(e.target.value);
+		updateData(
+			{
+				material: e.target.value,
+				chargenr: chargenr,
+				amount: amount,
+			},
+			data._id
+		);
+	};
+	const handleChargenrChange = (e) => {
+		setChargenr(e.target.value);
+		updateData(
+			{
+				material: material,
+				chargenr: e.target.value,
+				amount: amount,
+			},
+			data._id
+		);
+	};
+	const handleAmountChange = (e) => {
+		setAmount(e.target.value);
+		updateData(
+			{
+				material: material,
+				chargenr: chargenr,
+				amount: e.target.value,
+			},
+			data._id
+		);
+	};
+
+	const date = new Date(parseInt(data.time)).toLocaleDateString();
+	const time = new Date(parseInt(data.time)).toLocaleTimeString();
+	return (
+		<div className='DataRow'>
+			<input type='text' disabled value={data.index} />
+			<input type='text' value={material} onChange={handleMaterialChange} />
+			<input type='text' value={chargenr} onChange={handleChargenrChange} />
+			<input type='number' value={amount} onChange={handleAmountChange} />
+			<input type='text' disabled value={`${date + ' ' + time}`} />
+		</div>
+	);
+};
+
 const Desktop = ({data, setData}) => {
 	return (
 		<div className='Desktop'>
 			<p>Material Erfassung:</p>
 			<div className='Grid'>
 				<div className='Header'>
+					<input type='text' disabled placeholder='Order-Nr' />
 					<input type='text' disabled placeholder='Material' />
 					<input type='text' disabled placeholder='Changen-Nr.' />
 					<input type='text' disabled placeholder='Menge' />
+					<input type='text' disabled placeholder='Datum' />
 				</div>
 
-				{/*TODO: Map over DB data below*/}
-				<div className='Row'>
-					<input type='text' />
-					<input type='text' />
-					<input type='text' />
-				</div>
+				{
+					/*Map over DB data*/
+					data.map((row) => {
+						return <DataRow data={row} setData={setData} key={row._id} />;
+					})
+				}
 			</div>
 		</div>
 	);
 };
 
 export default function App() {
+	const [data, setData] = React.useState([]);
 	const [isMobile, setIsMobile] = React.useState(false);
+
 	React.useEffect(() => {
 		setIsMobile(window.innerWidth < 768);
 
@@ -93,13 +196,14 @@ export default function App() {
 		};
 	}, []);
 
-	const getDataFromDB = () => {
-		fetch(process.env.REACT_APP_API_URL + '/api/getData')
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data);
+	React.useEffect(() => {
+		if (!data.length) {
+			//get data from backend initially
+			getDataFromDB().then((data) => {
+				setData(data);
 			});
-	};
+		}
+	}, [data]);
 
-	return <div className='App'>{isMobile ? <Mobile /> : <Desktop />}</div>;
+	return <div className='App'>{isMobile ? <Mobile data={data} setData={setData} /> : <Desktop data={data} setData={setData} />}</div>;
 }
